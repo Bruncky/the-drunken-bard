7f9ce49bf67630b79f622bd3e1ace18853545f2b
Bruncky <brunobrunckhorst@gmail.com>
2020-02-20 15:43:38 +0100

    Coded CocktailsController

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,31 @@
+# frozen_string_literal: true
+
+# CocktailsController
 class CocktailsController < ApplicationController
+  before_action :set_task, only: %i[show]
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show; end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    Cocktail.create(cocktail_params)
+    redirect_to cocktail_path
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+
+  def set_task
+    @cocktail = Cocktail.find(params[:id])
+  end
 end
