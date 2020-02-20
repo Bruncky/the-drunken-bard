723130ca486c64cbcca27120d753496746c8de42
Bruncky <brunobrunckhorst@gmail.com>
2020-02-20 15:50:59 +0100

    Fixed routes for Cocktail

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :restaurants, only: %i[index show new create]
+  resources :cocktails, only: %i[index show new create]
 end
