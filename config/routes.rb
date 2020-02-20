7dcfca505739bb7846a9c3e48549ae7456213281
Bruncky <brunobrunckhorst@gmail.com>
2020-02-20 18:21:37 +0100

    Edited form, non-functional

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cocktails, only: %i[index show new create] do
-    resources :ingredients, only: %i[index]
+    resources :doses, only: %i[index]
   end
 end
