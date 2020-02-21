1ef944fe809526e0ab20b6a48f8a6527f6780832
Bruncky <brunobrunckhorst@gmail.com>
2020-02-21 11:31:01 +0100

    Added routes for Doses

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cocktails, only: %i[index show new create] do
-    resources :doses, only: %i[index]
+    resources :doses, only: %i[new create delete]
   end
 end
