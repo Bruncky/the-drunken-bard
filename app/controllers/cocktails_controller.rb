8c01d764daf2cc120e79e54ace67e04c65e7f353
Bruncky <brunobrunckhorst@gmail.com>
2020-02-23 18:18:51 +0100

    Added necessary functions to display placeholder image if user has not uploaded any

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
+require 'open-uri'
+
 # CocktailsController
 class CocktailsController < ApplicationController
   before_action :set_cocktail, only: %i[show]
   before_action :set_all_cocktails, only: %i[home index all]
+  before_action :set_default_photo, only: %i[create]
 
   def home; end
 
@@ -23,6 +26,10 @@ class CocktailsController < ApplicationController
   def create
     @cocktail = Cocktail.new(cocktail_params)
 
+    unless @cocktail.photo.attached?
+      @cocktail.photo.attach(io: @default, filename: 'default.jpg', content_type: 'image/jpeg')
+    end
+
     if @cocktail.save
       redirect_to cocktail_path(@cocktail)
     else
@@ -33,7 +40,7 @@ class CocktailsController < ApplicationController
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :photo)
   end
 
   def set_all_cocktails
@@ -42,5 +49,9 @@ class CocktailsController < ApplicationController
 
   def set_cocktail
     @cocktail = Cocktail.find(params[:id])
+  end
+
+  def set_default_photo
+    @default = URI.open('https://res.cloudinary.com/bruncky/image/upload/v1582473126/The%20Drunken%20Bard/default.jpg')
   end
 end
